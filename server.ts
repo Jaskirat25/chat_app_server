154b111cee9ed6537e617aa6b4504840b3b91844
jaskirat <jaskiratsingh251103@gmail.com>
2026-05-06 11:58:27 +0530

    Add tsconfig.json and fix ESM imports for Render deployment

--- server.ts
+++ server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
-import handleIoConnection from "./socket/index";
+import handleIoConnection from "./socket/index.ts";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000","https://chat-app-next-25.vercel.app/"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://chat-app-next-25.vercel.app/",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
