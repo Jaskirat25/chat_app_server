646efa3ad720f980dba4951ce70bae75e0505067
jaskirat <jaskiratsingh251103@gmail.com>
2026-05-06 13:06:05 +0530

    cors update

--- server.ts
+++ server.ts
@@ -1,27 +1,36 @@
 import express from "express";
-import { Server } from "socket.io";
+import cors from "cors";
 import http from "http";
-import handleIoConnection from "./socket/index.js";
+import { Server } from "socket.io";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://chat-app-next-25.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://chat-app-next-25.vercel.app",
-      "https://chat-app-next-25-git-master-jaskirat-singhs-projects-69da70b7.vercel.app"
+      "https://chat-app-next-25.vercel.app"
     ],
-    methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
-handleIoConnection(io);
-
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
