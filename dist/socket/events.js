995b830f41ac0d12ed0cf27dcc4c8e07b3a21ec1
jaskirat <jaskiratsingh251103@gmail.com>
2026-06-08 16:21:06 +0530

    removed dist from git

--- dist/socket/events.js
+++
@@ -1,5 +0,0 @@
-export function handleSocketEvents(io, socket) {
-    socket.on("send-message", ({ message, receiverId }) => {
-        io.to(receiverId).emit("receive-message", { from: socket.id, message });
-    });
-}
